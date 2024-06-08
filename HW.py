8174f760e4487f31d11754a0647546bf66bf4794
Elmir Bikbaev <integral@nxt.ru>
2024-06-08 18:25:46 +0500

    added a loop to populate the list with dictionaries

--- HW.py
+++ HW.py
@@ -6,3 +6,8 @@ data = pd.DataFrame({'whoAmI':lst})
 data.head()
 lst1 = []
 arr = data.to_numpy()
+for value in arr:
+    if value == 'robot':
+        lst1.append({'whoAmI_robot':1,'whoAmI_human':0})
+    else:
+        lst1.append({'whoAmI_robot':0,'whoAmI_human':1})
